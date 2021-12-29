bfc6d20056ed6ec7b964fedca19e0973d1bf7131
casavechia <mc@lionx.com.br>
2021-12-29 17:06:26 -0300

    passing the id parameter to select the correct user in the user resolver

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -3,11 +3,11 @@ const users = async (_: any, __: any, { fetch }: any) => {
   return users.json();
 };
 
-const user = () => {
-  return {
-    id: '1',
-    userName: 'Matheus Casavechia',
-  };
+const user = async (_: any, { id }: any, { fetch }: any) => {
+  const response = await fetch('http://localhost:3000/users/' + id);
+  const user = await response.json();
+  console.log(user);
+  return user;
 };
 
 export const userResolvers = {
